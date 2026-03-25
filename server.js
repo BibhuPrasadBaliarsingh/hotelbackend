dfae9395e39dda2ef55bbceae132201cd2a9f888
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2026-03-25 12:08:14 +0530

    updateserver

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000'||"https://hotel232.netlify.app", credentials: true }));
+app.use(cors({ origin: "https://hotel232.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
